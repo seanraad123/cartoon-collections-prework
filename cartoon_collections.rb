e1209c30bd29cc53cf800dd7b8157bbf3ffad3bd
Sean Conrad <seanconrad123@gmail.com>
2018-06-11 20:34:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,6 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find {|word| word == "cheddar"}
-  end
 end
 
 find_the_cheese
