0dba472e000a46fd2a64bf0a334df8c3e6cea6ff
Sean Conrad <seanconrad123@gmail.com>
2018-06-11 20:32:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,8 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find do |word|
-    word == "cheddar"
+    if word == "cheddar"
+      return true
+    end
   end
 end
