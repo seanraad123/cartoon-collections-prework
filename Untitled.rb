818433ef0af03ecbf346dc6ced16232a868bbd9f
Sean Conrad <seanconrad123@gmail.com>
2018-06-11 20:31:26 +0000

    automatically backed up by learn

---
+++ Untitled.rb
@@ -0,0 +1,5 @@
+names = ["bla", "car"]
+
+#puts names.map! {|name| name.capitalize } # now names contains ['Danil', 'Edmund']
+
+puts names.each { |name| name.capitalize } # here we just do something with each element
