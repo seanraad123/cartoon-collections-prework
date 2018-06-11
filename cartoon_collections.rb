8965b38eab9a3de37d21efc64212ae0f8169b291
Sean Conrad <seanconrad123@gmail.com>
2018-06-11 20:36:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find {|word| word == "cheddar"}
+  puts array.find {|word| word == "cheddar"}
 end
 
 cheese_types = ["cheddar", "gouda", "camembert"]
